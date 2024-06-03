7c9b86adc0bd8ad49f5081ae13862b2e679b3930
YanisGhernouz <25529071+Ghernouz@users.noreply.github.com>
2024-06-03 13:10:34 +0200

    add google microsoft signup button

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,35 @@
 import { NextAuthConfig } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from "next-auth/providers/google";
+import AzureAD from "next-auth/providers/microsoft-entra-id";
 
 const authConfig = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? '',
       clientSecret: process.env.GITHUB_SECRET ?? ''
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    }),
+    AzureAD({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: 'common',
+      token: {
+        url: `https://login.microsoftonline.com/common/oauth2/v2.0/token`,
+    },
+      userinfo: {
+        url: "https://graph.microsoft.com/oidc/userinfo",
+    },
+      authorization: {
+          url: `https://login.microsoftonline.com/common/oauth2/v2.0/authorize`,
+          params: {
+              scope: "openid profile email User.Read"
+          }
+      },
+      issuer: `https://login.microsoftonline.com/common/v2.0`
     })
   ],
   pages: {
---
+++ components/microsoft-auth-button.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import {  signIn } from 'next-auth/react';
+import { Button } from './ui/button';
+import { Icons } from './icons';
+
+export default function MicrosoftSignInButton() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl');
+
+  return (
+    <Button
+      className="w-full"
+      variant="outline"
+      type="button"
+      onClick={() =>
+        signIn('microsoft-entra-id', { callbackUrl: callbackUrl ?? '/dashboard' })
+      }
+    >
+      <Icons.microsoft className="mr-2 h-4 w-4" />
+      Continue with Microsoft
+    </Button>
+  );
+}
