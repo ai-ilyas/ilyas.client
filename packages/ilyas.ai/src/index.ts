12d86a8dcc3f43fec5c093bead5290c164ee889d
hocino <94870540+hocino@users.noreply.github.com>
2024-06-17 11:20:34 +0100

    Remove ilyas.ai package

--- packages/ilyas.ai/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { generateText } from "ai"
-import { openai } from "@ai-sdk/openai"
-
-export const getMermaidFromPrompt = async (prompt: string) => {
-    return await generateText({
-        model: openai("gpt-4-turbo"),
-        prompt: prompt
-        })
-}
