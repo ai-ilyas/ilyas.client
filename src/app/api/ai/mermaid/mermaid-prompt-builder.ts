100ab9fb1feaeba48f8952de85a4fd226e8e39af
hocino <94870540+hocino@users.noreply.github.com>
2024-06-20 09:31:57 +0100

    Remove useless instruction from prompt builder
    AB#27

--- src/app/api/ai/mermaid/mermaid-prompt-builder.ts
+++ src/app/api/ai/mermaid/mermaid-prompt-builder.ts
@@ -5,7 +5,7 @@ export const buildMermaidDiagramPrompt = (input: string): string => {
   }`;
 
   return `
-    You are an IT & Cloud architecture assistant. Your task is to generate an architecture diagram based on the following demand in <<<>>> represented in mermaid's syntax:
+    You are an IT & Cloud architecture assistant. Your task is to generate an architecture diagram based or any graph based on the following demand in <<<>>> represented in mermaid's syntax:
     When presented with the demand, come up with all the services and components necessary, list their respectives interactions.
     Afterward, combine all the information and generate a mermaid text-based diagram. Follow the step below
 
@@ -13,13 +13,6 @@ export const buildMermaidDiagramPrompt = (input: string): string => {
     Considering the demand above, follow the steps below to generate the required JSON output, The JSON schema should include: ${JSON_SCHEMAS}.
 
     # Instructions:
-    ## List components:
-    In clear and concise language, list components used in the architecture to adress the demand.
-    Only list components that are usefull and correspond to infrastructure components. Don't add extra-services that are equivalents.
-
-    ## List components interactions:
-    Using the list of components generate their necessary interactions(read/write) to adress the demand.
-    Make sure that it is technicaly possible and to only link components together that can and should interact together.
 
     ## Generate a Mermaid diagram:
     Using the list created before of components and their respectives interactions, create a text-based diagram representation of the architecture in Mermaid format with correct mermaid syntax.
