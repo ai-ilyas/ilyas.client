dc27d969e6fb8d16188ad784429713dea17b9b25
hocino <94870540+hocino@users.noreply.github.com>
2024-06-27 11:04:25 +0100

    Create API route to create application and return the ID
    AB#36

---
+++ src/app/api/application/create/route.ts
@@ -0,0 +1,11 @@
+import { auth } from '@/src/auth';
+import { insertApplication } from '@/src/lib/core/application/commands/insert-application';
+import { IApplication } from '@/src/lib/domain/entities/application.interface';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+  const applicationName = await req.text();
+  const session = (await auth())!;
+  const id = await insertApplication({ name: applicationName} as IApplication, session.user!.id!); 
+  return new NextResponse(id, { status: 200 });
+}
