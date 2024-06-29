69095e0ae99958e4fd30895aabe1e2c1b9c6d848
hocino <94870540+hocino@users.noreply.github.com>
2024-06-29 20:47:19 +0100

    Add protected route on middleware with internationalization + remove some spaces
    AB#62

--- src/lib/domain/repositories/unit-of-work.interface.ts
+++ src/lib/domain/repositories/unit-of-work.interface.ts
@@ -1,9 +1,9 @@
 import { IApplicationRepository } from "./application-repository.interface";
 
 export interface IUnitOfWork {
-    startTransaction(): Promise<void>;
-    commitTransaction(): Promise<void>;
-    abortTransaction(): Promise<void>;
-    applicationRepository: IApplicationRepository | undefined;
-  }
+  startTransaction(): Promise<void>;
+  commitTransaction(): Promise<void>;
+  abortTransaction(): Promise<void>;
+  applicationRepository: IApplicationRepository | undefined;
+}
   
