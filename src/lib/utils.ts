08071751729ab77eae04d14347b526ea9e8611b9
YanisGhernouz <25529071+Ghernouz@users.noreply.github.com>
2024-07-26 01:00:12 +0200

    AB#76 integration ai chatbot template( + gitignored packagelockjson)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,7 @@ import { twMerge } from 'tailwind-merge';
 import { Active, DataRef, Over } from '@dnd-kit/core';
 import { ColumnDragData } from '@/src/components/kanban/board-column';
 import { TaskDragData } from '@/src/components/kanban/task-card';
+import { customAlphabet } from 'nanoid'
 
 type DraggableData = ColumnDragData | TaskDragData;
 
@@ -39,3 +40,23 @@ export const isValidHtmlColor = (color: string): boolean => {
   const htmlColorRegex = /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
   return htmlColorRegex.test(color);
 }
+
+export const formatNumber = (value: number) =>
+  new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(value)
+
+export const runAsyncFnWithoutBlocking = (
+  fn: (...args: any) => Promise<any>
+) => {
+  fn()
+}
+
+export const sleep = (ms: number) =>
+  new Promise(resolve => setTimeout(resolve, ms))
+
+export const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  7
+) // 7-character random string
